74c277dd3ba1b3eb492f616d5dd4db9130052837
IMD <56916533+Innovae-Soporte@users.noreply.github.com>
2025-07-02 01:10:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,4 @@ def generate_presigned_url(req: PresignRequest):
         )
         return {"url": url}
     except Exception as e:
-    import traceback
-    traceback.print_exc()
-    raise HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
